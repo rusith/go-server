352187de215a6fc2be2923295d93f5177e459008
Rusith <rusith@mail.com>
2018-09-05 13:32:32 +0530

    An Extremely simple server to get started

--- go-server/main.go
+++ go-server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Main function invoked")	
+	http.HandleFunc("/", sayHello)
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		panic(err)
+	}
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	message := strings.TrimPrefix(r.URL.Path, "/")
+	message = "Hello " + message + "!"
+	w.Header().Set("Content-Type", "application/json")
+	jsonD, jsonErr := json.Marshal(message)
+	if jsonErr != nil {
+		w.Write([]byte("\"An error occurred while trying to process the request\""))
+		w.WriteHeader(500)
+	}
+	w.Write(jsonD)
 }
